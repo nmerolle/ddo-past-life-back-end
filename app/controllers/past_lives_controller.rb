b1412eb8cd5c38c41658fc69e678c9d1934372aa
nmerolle <nmerolle11@hotmail.com>
2021-10-01 00:06:41 -0400

    updated controllers

--- app/controllers/past_lives_controller.rb
+++ app/controllers/past_lives_controller.rb
@@ -1,47 +1,47 @@
 class PastLivesController < ApplicationController
-  before_action :set_past_life, only: [:show, :update, :destroy]
+  before_action :set_past_life, only: [:update, :destroy]
 
   # GET /past_lives
   def index
-    @past_lives = PastLife.all
-
-    render json: @past_lives
+    past_lives = Character.find_by(id: params[:character_id]).past_lives
+    render json: past_lives
   end
 
   # GET /past_lives/1
-  def show
-    render json: @past_life
-  end
+  #def show
+  #  render json: @past_life
+  #end
 
   # POST /past_lives
   def create
-    @past_life = PastLife.new(past_life_params)
+    character = Character.find_by(id: params(:character_id))
+    past_life = character.past_lives.build(past_life_params)
 
-    if @past_life.save
-      render json: @past_life, status: :created, location: @past_life
+    if past_life.save
+      render json: past_life, status: :created, location: past_life
     else
-      render json: @past_life.errors, status: :unprocessable_entity
+      render json: past_life.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /past_lives/1
   def update
-    if @past_life.update(past_life_params)
-      render json: @past_life
+    if past_life.update(past_life_params)
+      render json: past_life
     else
-      render json: @past_life.errors, status: :unprocessable_entity
+      render json: past_life.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /past_lives/1
   def destroy
-    @past_life.destroy
+    past_life.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_past_life
-      @past_life = PastLife.find(params[:id])
+      past_life = PastLife.find_by(id: params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
  
-  resources :characters, only: [:index, :create, :update, :delete] do
-    resources :past_lives, only: [:index, :create, :update, :delete]
+  resources :characters, only: [:index, :create, :update, :destroy] do
+    resources :past_lives, only: [:index, :create, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
